7de4ff21a556186e54756b11a7d6ecc256552870
4nandaw <ananda.vilar.vidal@ccc.ufcg.edu.br>
2026-02-20 12:24:59 -0300

    feat: wrap MainLayout with ProductProvider to manage product state alongside CartProvider

--- frontend/src/app/components/layout/MainLayout.jsx
+++ frontend/src/app/components/layout/MainLayout.jsx
@@ -2,23 +2,26 @@ import { Outlet } from "react-router";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { CartProvider } from "../../context/CartContext";
+import { ProductProvider } from "../../context/ProductContext";
 import { Toaster } from "sonner";
 
 export function MainLayout() {
   return (
-    <CartProvider>
-      <div className="flex min-h-screen flex-col bg-slate-50 font-sans">
-        <Header />
-        <main className="flex-1 w-full">
-          <Outlet />
-        </main>
-        <Footer />
-        <Toaster 
-          position="top-right"
-          richColors
-          expand={false}
-        />
-      </div>
-    </CartProvider>
+    <ProductProvider>
+      <CartProvider>
+        <div className="flex min-h-screen flex-col bg-slate-50 font-sans">
+          <Header />
+          <main className="flex-1 w-full">
+            <Outlet />
+          </main>
+          <Footer />
+          <Toaster 
+            position="top-right"
+            richColors
+            expand={false}
+          />
+        </div>
+      </CartProvider>
+    </ProductProvider>
   );
 }
